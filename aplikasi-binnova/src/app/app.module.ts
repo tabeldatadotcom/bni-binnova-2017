397f62a955e6b391b5a4e27eff1bf8dce026fb57
VALERIANPC <leryev@gmail.com>
2017-09-21 13:16:38 +0700

    Update form

--- aplikasi-binnova/src/app/app.module.ts
+++ aplikasi-binnova/src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AppDaftar} from "./app.daftar";
+import { AppNilai } from './app.nilai';
+
+const appRoutes: Routes = [
+  { path: 'component', component: AppComponent },
+  { path: 'daftar', component: AppDaftar}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppDaftar,
+    AppNilai
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing:true}
+    )
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    AppDaftar,
+    AppNilai]
 })
+
 export class AppModule { }
