00dfffff1f65867531b4f28c0cb3014dcf165a53
VALERIANPC <leryev@gmail.com>
2017-09-21 13:17:44 +0700

    Update form

---
+++ aplikasi-binnova/src/app/app.nilai.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.nilai.html',
+  styleUrls: ['./app.nilai.css']
+})
+export class AppNilai{
+
+}
